d1cb2ceb5338f2db89aea555774817b7bc40240b
Hugo Landeros <hugo.landeros@valunet.mx>
2026-03-12 20:19:58 -0600

    feat: Build first Astro Island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({ site: "https://lucent-figolla-935c9d.netlify.app/" });
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
+export default defineConfig({
+  site: "https://lucent-figolla-935c9d.netlify.app/",
+  integrations: [preact()],
+});
